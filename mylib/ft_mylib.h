70314f26e3d18d2250e5c2f1e68ca482364b51f9
Erchegyi David <edavid@2-A-1.42heilbronn.de>
2021-11-27 19:37:02 +0100

    quick fix

---
+++ mylib/ft_mylib.h
@@ -0,0 +1,19 @@
+#ifndef FT_MYLIB_H
+# define FT_MYLIB_H
+
+# include "headers/ft_libftctype.h"
+# include "headers/ft_libftdlist.h"
+# include "headers/ft_libftextra.h"
+# include "headers/ft_libftfilelist.h"
+# include "headers/ft_libftgnl.h"
+# include "headers/ft_libftmath.h"
+# include "headers/ft_libftmemory.h"
+# include "headers/ft_libftobjlist.h"
+# include "headers/ft_libftprint.h"
+# include "headers/ft_libftput.h"
+# include "headers/ft_libftslist.h"
+# include "headers/ft_libftsort.h"
+# include "headers/ft_libftstring.h"
+# include "headers/ft_libftvalid.h"
+
+#endif /* FT_MYLIB_H */
