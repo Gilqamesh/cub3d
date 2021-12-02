8e6fddca16283d32b36764d7d2a3590577a7c081
FunkyOctopus <>
2021-12-02 17:22:00 +0100

    norm

--- sources/updating_functions/update_key.c
+++ sources/updating_functions/update_key.c
@@ -53,29 +53,47 @@ double y)
 	}
 }
 
+static void	key_left(t_cub3D *mystruct,
+	double oldDirX, double oldPlaneX)
+{
+	oldDirX = mystruct->dirX;
+	mystruct->dirX = mystruct->dirX * cos(ROTATE_SPEED)
+		- mystruct->dirY * sin(ROTATE_SPEED);
+	mystruct->dirY = oldDirX * sin(ROTATE_SPEED)
+		+ mystruct->dirY * cos(ROTATE_SPEED);
+	oldPlaneX = mystruct->planeX;
+	mystruct->planeX = mystruct->planeX * cos(ROTATE_SPEED)
+		- mystruct->planeY * sin(ROTATE_SPEED);
+	mystruct->planeY = oldPlaneX * sin(ROTATE_SPEED)
+		+ mystruct->planeY * cos(ROTATE_SPEED);
+}
+
+static void	key_right(t_cub3D *mystruct,
+	double oldDirX, double oldPlaneX)
+{
+	oldDirX = mystruct->dirX;
+	mystruct->dirX = mystruct->dirX * cos(-ROTATE_SPEED)
+		- mystruct->dirY * sin(-ROTATE_SPEED);
+	mystruct->dirY = oldDirX * sin(-ROTATE_SPEED)
+		+ mystruct->dirY * cos(-ROTATE_SPEED);
+	oldPlaneX = mystruct->planeX;
+	mystruct->planeX = mystruct->planeX * cos(-ROTATE_SPEED)
+		- mystruct->planeY * sin(-ROTATE_SPEED);
+	mystruct->planeY = oldPlaneX * sin(-ROTATE_SPEED)
+		+ mystruct->planeY * cos(-ROTATE_SPEED);
+}
+
 static void	handle_left_right(int key, t_cub3D *mystruct)
 {
 	double	oldDirX;
 	double	oldPlaneX;
 
+	oldDirX = 0.0;
+	oldPlaneX = 0.0;
 	if (key == KEY_RIGHT)
-	{
-		oldDirX = mystruct->dirX;
-		mystruct->dirX = mystruct->dirX * cos(-ROTATE_SPEED) - mystruct->dirY * sin(-ROTATE_SPEED);
-		mystruct->dirY = oldDirX * sin(-ROTATE_SPEED) + mystruct->dirY * cos(-ROTATE_SPEED);
-		oldPlaneX = mystruct->planeX;
-		mystruct->planeX = mystruct->planeX * cos(-ROTATE_SPEED) - mystruct->planeY * sin(-ROTATE_SPEED);
-		mystruct->planeY = oldPlaneX * sin(-ROTATE_SPEED) + mystruct->planeY * cos(-ROTATE_SPEED);
-	}
+		key_right(mystruct, oldDirX, oldPlaneX);
 	else if (key == KEY_LEFT)
-	{
-		oldDirX = mystruct->dirX;
-		mystruct->dirX = mystruct->dirX * cos(ROTATE_SPEED) - mystruct->dirY * sin(ROTATE_SPEED);
-		mystruct->dirY = oldDirX * sin(ROTATE_SPEED) + mystruct->dirY * cos(ROTATE_SPEED);
-		oldPlaneX = mystruct->planeX;
-		mystruct->planeX = mystruct->planeX * cos(ROTATE_SPEED) - mystruct->planeY * sin(ROTATE_SPEED);
-		mystruct->planeY = oldPlaneX * sin(ROTATE_SPEED) + mystruct->planeY * cos(ROTATE_SPEED);
-	}
+		key_left(mystruct, oldDirX, oldPlaneX);
 }
 
 void	update_position_by_key(t_cub3D *mystruct, int key)
@@ -83,8 +101,10 @@ void	update_position_by_key(t_cub3D *mystruct, int key)
 	double	x;
 	double	y;
 
-	x = mystruct->dirX / euclidean_distance(mystruct->dirX, mystruct->dirY) * MOVE_SPEED;
-	y = mystruct->dirY / euclidean_distance(mystruct->dirX, mystruct->dirY) * MOVE_SPEED;
+	x = mystruct->dirX
+		/ euclidean_distance(mystruct->dirX, mystruct->dirY) * MOVE_SPEED;
+	y = mystruct->dirY
+		/ euclidean_distance(mystruct->dirX, mystruct->dirY) * MOVE_SPEED;
 	if (key == KEY_W || key == KEY_S)
 		handle_forward_backward(key, mystruct, x, y);
 	else if (key == KEY_A || key == KEY_D)
